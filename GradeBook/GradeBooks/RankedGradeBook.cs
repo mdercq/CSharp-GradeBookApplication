26c641b1b47c134de8c671617679bd4650d8bfd8
Dercq Michael <mdercq@gmail.com>
2020-09-11 19:10:05 +0200

    dev: create GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,15 +1,35 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook : BaseGradeBook
+    class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+
+        /// <summary>
+        /// Ranked-grading grades students not based on their individual performance, but rather their performance compared to their peers. This means the 20% of the students with the highest grade of a class get an A, the next highest 20% get a B, etc. There are many ways to calculate this. I'd recommend figuring out how many students it takes to drop a letter grade (20% of the total number of students) X, put all the average grades in order, then figure out where the input grade would fit in the series of grades. Every X students with higher grades than the input grade knocks the letter grade down until you reach F.
+        /// </summary>
+        /// <param name="averageGrade"></param>
+        /// <returns>char: Grade as letter [A->F]</returns>
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            
+            int twentyPercent = Students.Count / 5;
+            int studentsBetterThanThis = Students.Where(x => x.AverageGrade > averageGrade).Count();
+            if (studentsBetterThanThis < twentyPercent) return 'A';
+            else if (studentsBetterThanThis < twentyPercent * 2) return 'B';
+            else if (studentsBetterThanThis < twentyPercent * 3) return 'C';
+            else if (studentsBetterThanThis < twentyPercent * 4) return 'D';
+
+            return 'F';
         }
     }
 }
